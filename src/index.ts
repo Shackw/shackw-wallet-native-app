179a5999e93f7f6109576e9d2b4e5d46995f1904
Kazuki Kameyama <kame@ficklewolf.com>
2025-09-07 16:39:24 +0900

    refactor: Support for upgrading gluestack-ui

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import "react-native-url-polyfill/auto";
 // @ts-ignore
 import { Buffer as NodeBuffer } from "buffer";
 
+import "@/styles/global.css";
+
 if (typeof global.Buffer === "undefined") {
   // @ts-ignore
   global.Buffer = NodeBuffer;
