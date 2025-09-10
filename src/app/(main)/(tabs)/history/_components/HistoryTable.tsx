858b5271516cc97f1d62cb11167009ab8e4d9576
Kazuki Kameyama <kame@ficklewolf.com>
2025-09-10 21:59:29 +0900

    feat: Creating an Anchor component Revising the design of the transaction history screen

--- src/app/(main)/(tabs)/history/_components/HistoryTable.tsx
+++ src/app/(main)/(tabs)/history/_components/HistoryTable.tsx
@@ -1,8 +1,16 @@
+import { ScrollView } from "react-native";
+
+import Anchor from "@/components/Anchor";
+import { DEFAULT_CHAIN } from "@/configs/chain";
+import { shortenAddress } from "@/helpers/address";
 import { Token } from "@/registries/TokenRegistry";
 import { theme } from "@/styles/theme";
+import { Box } from "@/vendor/gluestack-ui/box";
+import { HStack } from "@/vendor/gluestack-ui/hstack";
 import { Spinner } from "@/vendor/gluestack-ui/spinner";
-import { Table, TableBody, TableData, TableRow } from "@/vendor/gluestack-ui/table";
+import { Table, TableBody, TableRow } from "@/vendor/gluestack-ui/table";
 import { Text } from "@/vendor/gluestack-ui/text";
+import { VStack } from "@/vendor/gluestack-ui/vstack";
 
 import useHistoryRows, { HistoryTerm } from "../_hooks/useHistoryRows";
 
@@ -15,24 +23,63 @@ const HistoryTable = (props: HistoryTableProps) => {
     ...term
   });
 
-  if (!historyRows && isError) return <Text>エラーが発生しました。</Text>;
+  if (!historyRows && isError)
+    return (
+      <VStack className="flex-1 justify-center items-center pb-32">
+        <Text>エラーが発生しました。</Text>
+      </VStack>
+    );
 
-  if (!historyRows) return <Spinner color={theme.colors.primary[400]} size={34.3} />;
+  if (!historyRows)
+    return (
+      <VStack className="flex-1 justify-center items-center pb-32">
+        <Spinner color={theme.colors.primary[400]} size={34.3} />
+      </VStack>
+    );
 
-  if (historyRows.length === 0) return <Text>データがありません。</Text>;
+  if (historyRows.length === 0)
+    return (
+      <VStack className="flex-1 justify-center items-center pb-32">
+        <Text>データがありません。</Text>
+      </VStack>
+    );
+
+  console.log(DEFAULT_CHAIN.blockExplorers);
 
   return (
-    <Table className="w-full overflow-y-auto">
-      <TableBody>
-        {historyRows.map((row, index) => (
-          <TableRow key={`${row.token}-history-${index}`}>
-            <TableData>{row.counterparty}</TableData>
-            <TableData>{`${row.value}${row.token}`}</TableData>
-            <TableData>{row.transactionAt}</TableData>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+    <ScrollView style={{ flex: 1 }} showsVerticalScrollIndicator>
+      <Table className="w-full overflow-y-auto">
+        <TableBody className="w-full">
+          {historyRows.map(({ txHash, token, value, counterparty, direction, transactionAt }, index) => (
+            <TableRow key={`${token}-history-${index}`} className="w-full">
+              <HStack className="w-full py-2">
+                <Box
+                  className="h-full w-1.5 rounded-full"
+                  style={{ backgroundColor: direction === "IN" ? "#A3D8F6" : "#FFA3B5" }}
+                />
+                <VStack className="w-full py-1 pl-3 pr-6 gap-y-1">
+                  <HStack className="justify-between">
+                    <Text className="font-bold text-secondary-500">{transactionAt}</Text>
+                    {DEFAULT_CHAIN.blockExplorers?.default ? (
+                      <Anchor
+                        href={`${DEFAULT_CHAIN.blockExplorers.default.url}/tx/${txHash}`}
+                        className="font-bold text-secondary-500"
+                      >{`ID: ${shortenAddress(txHash, 5)}`}</Anchor>
+                    ) : (
+                      <Text className="font-bold text-secondary-500">{`ID: ${shortenAddress(txHash, 5)}`}</Text>
+                    )}
+                  </HStack>
+                  <Text className="font-bold" size="lg">
+                    {shortenAddress(counterparty, 12)}
+                  </Text>
+                  <Text className="font-bold text-right" size="xl">{`${value} ${token}`}</Text>
+                </VStack>
+              </HStack>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </ScrollView>
   );
 };
 
