9451e0d150b70d7461beba7dcef9c58902e206d7
Kazuki Kameyama <kameyama1220h@gmail.com>
2025-10-13 21:52:19 +0900

    feat(QR): Added QR code sharing function

--- src/components/Addresses/AddressesDisplayQR.tsx
+++ src/components/Addresses/AddressesDisplayQR.tsx
@@ -1,16 +1,18 @@
-import { setStringAsync } from "expo-clipboard";
 import { useCallback, useRef } from "react";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import { Address } from "viem";
 
+import { useBoolean } from "@/hooks/useBoolean";
 import { Box } from "@/vendor/gluestack-ui/box";
 import { HStack } from "@/vendor/gluestack-ui/hstack";
 import { Text } from "@/vendor/gluestack-ui/text";
 import { VStack } from "@/vendor/gluestack-ui/vstack";
 
 import { ContainButton } from "../Button";
+import { AlertDialog } from "../Dialog";
 import { BottomInputDrawer } from "../Drawer";
-import QRCode from "../QRCode";
+import QRCode, { QRCodeHandle } from "../QRCode";
+import { ErrorText } from "../Text";
 
 type AddressesDisplayQRProps = {
   name: string;
@@ -20,42 +22,55 @@ type AddressesDisplayQRProps = {
 
 const AddressesDisplayQR = (props: AddressesDisplayQRProps) => {
   const { name, address, componentProps } = props;
+  const [isShowErrorDialog, setIsShowErrorDialog] = useBoolean(false);
 
-  const shareLinkRef = useRef<string>("");
+  const qrCodeRef = useRef<QRCodeHandle | null>(null);
 
-  const handleShare = useCallback(() => {
-    setStringAsync(shareLinkRef.current);
+  const handleShareAddress = useCallback(async () => {
+    try {
+      await qrCodeRef.current?.handleShare();
+    } catch (err) {
+      console.log(err);
+    }
   }, []);
 
   return (
-    <BottomInputDrawer {...componentProps}>
-      <KeyboardAwareScrollView
-        contentContainerStyle={{ flexGrow: 1 }}
-        enableOnAndroid
-        keyboardShouldPersistTaps="handled"
-      >
-        <VStack className="flex-1 w-full items-center py-4 gap-y-6">
-          <VStack className="w-full gap-y-2">
-            <HStack className="gap-x-2">
-              <Box className="w-1.5 bg-primary-500" />
-              <Text className="font-bold">アドレス情報</Text>
-            </HStack>
-            <VStack className="border-[0.5px] border-secondary-300 px-5 py-6 bg-secondary-50 rounded-xl">
-              <VStack className="gap-y-1">
-                <Text className="font-bold text-secondary-600">名前</Text>
-                <Text className="font-bold text-secondary-800 text-right">{name}</Text>
-              </VStack>
-              <VStack className="gap-y-1">
-                <Text className="font-bold text-secondary-600">アドレス</Text>
-                <Text className="font-bold text-secondary-800 text-right">{address}</Text>
+    <>
+      <BottomInputDrawer {...componentProps}>
+        <KeyboardAwareScrollView
+          contentContainerStyle={{ flexGrow: 1 }}
+          enableOnAndroid
+          keyboardShouldPersistTaps="handled"
+        >
+          <VStack className="flex-1 w-full items-center py-4 gap-y-6">
+            <VStack className="w-full gap-y-2">
+              <HStack className="gap-x-2">
+                <Box className="w-1.5 bg-primary-500" />
+                <Text className="font-bold">アドレス情報</Text>
+              </HStack>
+              <VStack className="border-[0.5px] border-secondary-300 px-5 py-6 bg-secondary-50 rounded-xl">
+                <VStack className="gap-y-1">
+                  <Text className="font-bold text-secondary-600">名前</Text>
+                  <Text className="font-bold text-secondary-800 text-right">{name}</Text>
+                </VStack>
+                <VStack className="gap-y-1">
+                  <Text className="font-bold text-secondary-600">アドレス</Text>
+                  <Text className="font-bold text-secondary-800 text-right">{address}</Text>
+                </VStack>
               </VStack>
             </VStack>
+            <QRCode ref={qrCodeRef} path="addresses" query={{ name, address }} size={275} />
+            <ContainButton text="共有" size="lg" className="w-full mt-auto" onPress={handleShareAddress} />
           </VStack>
-          <QRCode ref={shareLinkRef} path="addresses" query={{ name, address }} size={275} />
-          <ContainButton text="共有" size="lg" className="w-full mt-auto" onPress={handleShare} />
+        </KeyboardAwareScrollView>
+      </BottomInputDrawer>
+
+      <AlertDialog title="QRコード共有の失敗" isOpen={isShowErrorDialog} onClose={setIsShowErrorDialog.off} size="lg">
+        <VStack className="py-4 gap-y-2">
+          <ErrorText className="flex-1">不明なエラーによりQRコードの共有に失敗しました。</ErrorText>
         </VStack>
-      </KeyboardAwareScrollView>
-    </BottomInputDrawer>
+      </AlertDialog>
+    </>
   );
 };
 
--- src/components/QRCode.tsx
+++ src/components/QRCode.tsx
@@ -1,8 +1,12 @@
-import { RefObject, useEffect, useMemo } from "react";
+import { File, Paths } from "expo-file-system";
+import * as Sharing from "expo-sharing";
+import { forwardRef, useCallback, useImperativeHandle, useMemo, useRef } from "react";
 import RNQRCode from "react-native-qrcode-svg";
+import Svg from "react-native-svg";
 import { Address } from "viem";
 
 import { ENV } from "@/configs/env";
+import { useBoolean } from "@/hooks/useBoolean";
 import { Token } from "@/registries/TokenRegistry";
 
 type QRCodeProps = (
@@ -11,10 +15,15 @@ type QRCodeProps = (
       path: "transfer";
       query: { sendToken: Token; feeToken: Token; recipient: Address; amount: number; webhookUrl?: string };
     }
-) & { ref: RefObject<string>; size: number };
+) & { size: number };
 
-const QRCode = (props: QRCodeProps) => {
-  const { size, path, query, ref } = props;
+export type QRCodeHandle = { handleShare: () => Promise<void> };
+
+const QRCode = forwardRef<QRCodeHandle, QRCodeProps>((props, ref) => {
+  const { size, path, query } = props;
+  const [isSharing, setIsSharing] = useBoolean(false);
+
+  const qrRef = useRef<Svg>(null);
 
   const value = useMemo(() => {
     const url = new URL(path, `${ENV.HINOMARU_UNIVERSAL_LINK}/wallet`);
@@ -22,11 +31,51 @@ const QRCode = (props: QRCodeProps) => {
     return url.toString();
   }, [path, query]);
 
-  useEffect(() => {
-    if (ref) ref.current = value;
-  }, [ref, value]);
+  const handleShare = useCallback(async () => {
+    if (isSharing) return;
 
-  return <RNQRCode value={value} size={size} ecl="M" backgroundColor="#fff" />;
-};
+    setIsSharing.on();
+    try {
+      qrRef.current?.toDataURL(async data => {
+        const file = new File(Paths.cache, "qrcode.png");
+        file.create({ overwrite: true });
+        file.write(data, { encoding: "base64" });
+
+        const isAvailable = await Sharing.isAvailableAsync();
+        if (!isAvailable) throw new Error("共有機能が利用できません");
+
+        await Sharing.shareAsync(file.uri, {
+          mimeType: "image/png",
+          dialogTitle: "QRコードを共有",
+          UTI: "public.png"
+        });
+      });
+    } catch {
+      throw new Error("QRコードの共有に失敗しました。");
+    } finally {
+      await new Promise(resolve => setTimeout(resolve, 500));
+      setIsSharing.off();
+    }
+  }, [isSharing, setIsSharing]);
+
+  useImperativeHandle(ref, () => ({ handleShare }), [handleShare]);
+
+  return (
+    <RNQRCode
+      logoMargin={-8}
+      logoSize={50}
+      logoBorderRadius={100}
+      quietZone={8}
+      logo={require("@/assets/images/splash.png")}
+      value={value}
+      size={size}
+      ecl="M"
+      backgroundColor="#fff"
+      getRef={ref => (qrRef.current = ref!)}
+    />
+  );
+});
+
+QRCode.displayName = "QRCode";
 
 export default QRCode;
