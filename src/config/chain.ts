c84934057c44a2a632c4a988f58d18da8496e06e
Kazuki Kameyama <kameyama1220h@gmail.com>
2025-11-25 23:50:41 +0900

    feat(chain): Fixed chain icon to be displayed on the up bar

--- src/config/chain.ts
+++ src/config/chain.ts
@@ -1,4 +1,15 @@
+import { ComponentType } from "react";
 import { mainnet, base, sepolia, baseSepolia, polygon, polygonAmoy, Chain as ViemChain } from "viem/chains";
+
+import {
+  BaseIcon,
+  BaseSepoliaIcon,
+  ChainlIconProps,
+  EthereumIcon,
+  PolygonAmoyIcon,
+  PolygonIcon,
+  SepoliaIcon
+} from "@/presentation/components/Icons/ChainIcons";
 
 export type Chain = (typeof CHAIN_KEYS)[number];
 
@@ -7,7 +18,7 @@ export const CHAIN_KEYS = ["mainnet", "base", "polygon", "sepolia", "baseSepolia
 export const CHAIN_KEY_TO_DISPLAY_NAME = {
   mainnet: "Ethereum Mainnet",
   base: "Base Mainnet",
-  polygon: "Polygon PoS",
+  polygon: "Polygon Mainnet",
   sepolia: "Ethereum Sepolia Testnet",
   baseSepolia: "Base Sepolia Testnet",
   polygonAmoy: "Polygon Amoy Testnet"
@@ -20,6 +31,15 @@ export const CHAINS: Record<Chain, ViemChain> = {
   sepolia,
   baseSepolia,
   polygonAmoy
+};
+
+export const CHAIN_ICONS: Record<Chain, ComponentType<ChainlIconProps>> = {
+  mainnet: EthereumIcon,
+  base: BaseIcon,
+  polygon: PolygonIcon,
+  sepolia: SepoliaIcon,
+  baseSepolia: BaseSepoliaIcon,
+  polygonAmoy: PolygonAmoyIcon
 };
 
 export const CHAIN_IDS = Object.values(CHAINS).map(c => c.id);
--- src/presentation/components/AppBar/AppBarBody/AppBarDefaultBody.tsx
+++ src/presentation/components/AppBar/AppBarBody/AppBarDefaultBody.tsx
@@ -1,17 +1,26 @@
+import { CHAIN_ICONS } from "@/config/chain";
 import { HStack } from "@/presentation/components/gluestack-ui/hstack";
 import { Spinner } from "@/presentation/components/gluestack-ui/spinner";
 import { Text } from "@/presentation/components/gluestack-ui/text";
 import { useShackwWalletContext } from "@/presentation/providers/ShackwWalletProvider";
+import { useWalletPreferencesContext } from "@/presentation/providers/WalletPreferencesProvider";
 import { shortenAddress } from "@/shared/helpers/address";
 
 const AppBarDefaultBody = () => {
   const { account } = useShackwWalletContext();
+  const { currentChain } = useWalletPreferencesContext();
+
   const accountAddress = account?.address;
+
+  const ChainIcon = CHAIN_ICONS[currentChain];
 
   return (
     <HStack className="w-full justify-center">
       {accountAddress ? (
-        <Text className="font-bold text-white">{shortenAddress(accountAddress)}</Text>
+        <HStack className="items-center gap-x-2">
+          <ChainIcon />
+          <Text className="font-bold text-white">{shortenAddress(accountAddress)}</Text>
+        </HStack>
       ) : (
         <Spinner size="small" color="white" />
       )}
